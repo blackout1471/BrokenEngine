0dcade4b3df4900825aa40ad1902b8515b5d66d3
Peter Cholub <ripiter15@wp.pl>
2019-05-09 15:37:22 +0200

    Initial commit

--- Test/Program.cs
+++ Test/Program.cs
@@ -16,6 +16,7 @@ namespace Test
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //Hello World
         }
     }
 }
